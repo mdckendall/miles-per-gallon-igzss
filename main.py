32912c27d671519dc50258793e787aedff88f923
Irene Gonzalez <123407062+igzss@users.noreply.github.com>
2023-04-13 23:25:46 +0000

    Printed "gallons used: "

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #your code here!
 M = float(input("Miles: "))
 G = float(input("Gallons: "))
-print(M*G)
+print("Gallons used: ",M*G)
 #read in the miles and read in the gallons used.
 
 #display the miles per gallon
